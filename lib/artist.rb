2050da14f5473cc18068b641ffb297d7345581af
Milan Mody <milanm1120@gmail.com>
2021-03-18 00:07:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,13 @@
 class Artist
   attr_accessor :name       #instance method, it will operate on the instance of this class and not the class itself.
+  attr_reader :song
 
   def initialize(name)
     @name = name
+    @song = []
   end
+
+  def song(song)
+    @song << song
+
 end
