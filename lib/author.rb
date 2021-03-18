28c3118e14858c934eb68c89983099a67aa1293f
Milan Mody <milanm1120@gmail.com>
2021-03-18 00:29:09 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,15 @@
 class Author
   attr_accessor :name
+  @@post_count = 0
 
-end 
+  def initialize(name)
+    @name = name
+    @post = []
+  end
+
+  def post
+    Post.all.select do |post|
+      post.author = self
+    end
+
+end
