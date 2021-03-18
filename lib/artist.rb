e116cb23160594bea4eb4f00e6beb410c48ceb6a
Milan Mody <milanm1120@gmail.com>
2021-03-18 00:04:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,7 @@
 class Artist
-  attr_accessor :name       #instance method, it will operate on the instance of this class and not the class itself. 
+  attr_accessor :name       #instance method, it will operate on the instance of this class and not the class itself.
 
+  def initialize(name)
+    @name = name
+  end
 end
